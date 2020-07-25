4eecfbf77b2898b427db3a2323a32abda16b272a
unknown <adityanavarch@gmail.com>
2020-07-25 22:57:59 +0530

    Added more code in '11-Getting User Input'

--- 11 - Getting User Input/src/App.java
+++ 11 - Getting User Input/src/App.java
@@ -7,12 +7,35 @@ public class App {
         Scanner input = new Scanner(System.in);
          
         // Output the prompt
+        System.out.println("Enter an integer value: ");
+         
+        // Wait for the user to enter something.
+        int  intvalue = input.nextInt();
+
         System.out.println("Enter a floating point value: ");
          
         // Wait for the user to enter something.
-        double value = input.nextDouble();
+        double doublevalue = input.nextDouble();
+         
+        System.out.println("Enter a word: ");
+         
+        // Wait for the user to enter something.
+        String wordvalue = input.next();
+        
+        input.nextLine();
+         /* Expecting only a word from the previous user input.
+        Therefore, moving on to the next line...... */
+
+        System.out.println("Enter a line of text: ");
+         
+        // Wait for the user to enter something.
+        String textvalue = input.nextLine();
+          
          
         // Tell them what they entered.
-        System.out.println("You entered: " + value);
+        System.out.println("The integer value you entered: " + intvalue);
+        System.out.println("The floating point value you entered: " + doublevalue);
+        System.out.println("The word you entered: " + wordvalue);
+        System.out.println("The line of text you entered: " + textvalue);
     }
 }
