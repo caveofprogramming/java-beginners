a9b7003c7232c2089186aad52ccc632fe1bd7e71
SACHIN LODHI <71810750+sachin3606@users.noreply.github.com>
2020-10-01 12:44:02 +0530

    Update Application.java

--- 05 - A Hello World Program/src/Application.java
+++ 05 - A Hello World Program/src/Application.java
@@ -1,3 +1,8 @@
+/*Points to remember =>
+1.) Make sure that you have installed the jdk and jre file and set path in Windows(before runn the program)
+2.) When we use public keyword with class the class and the file should be same(case-sensitive) or if we use class we can use different file name
+3.) Naming convention for class is all the letter should be in Capital. False naming convention will not give error, it make easy to identify the things.
+*/
 public class Application {
  
     public static void main(String[] args) {
