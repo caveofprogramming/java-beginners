e57caa18c3e05bf76c931d9bb39671462fa2ca17
SACHIN LODHI <71810750+sachin3606@users.noreply.github.com>
2020-10-01 13:06:41 +0530

    Update Application.java

--- 09 - For Loops/src/Application.java
+++ 09 - For Loops/src/Application.java
@@ -1,8 +1,16 @@
 public class Application {
     public static void main(String[] args) {
          
-        for(int i=0; i < 5; i++) {
+        for(int i=0; i < 5; i++) {//you can also create a infinite loop with for(;;)
             System.out.printf("The value of i is: %d\n", i);
         }
     }
 }
+
+/*Expected output =>
+The value of i is: 0
+The value of i is: 1
+The value of i is: 2
+The value of i is: 3
+The value of i is: 4
+*/
